93f0b4d935ae85d691b9a1dfaa819c435fe7e6fe
MaryamMuhammed5050 <maryammoh2050@gmail.com>
2026-09-16 16:50:44 +0100

    trigger and actions

--- main.py
+++ main.py
@@ -5,16 +5,20 @@ def save_request(trigger):
         print(f"your request has been saved: {trigger}")
 
 def send_email(trigger):
-    print(f"a confirmation email has been sent to you: {order}")
+    print(f"a confirmation email has been sent to you: {trigger}")
+
+    with open("email.txt", "a") as file:
+        file.write(trigger + "\n")
+        print(f"your email has been saved: {trigger}")
 
 
 def create_order(trigger):
     order = input("what would you like to order?\n")
-    print(f"your order has been taken: {order}")
+    print(f"your order has been taken: {trigger}")
 
     with open("order.txt", "a") as file:
         file.write(order + "\n")
-        print(f"your order has been saved: {order}")
+        print(f"your order has been saved: {trigger}")
 
 
 
@@ -24,16 +28,29 @@ while True:
     trigger = input("what do you want to do?\n")
     if trigger == "exit":
         break
+    action_taken = False
+    actions = []
+
+     
+
     if "workflow" in trigger:
+        save_request(trigger)
+        actions.append("workflow")
         print(f"Your request has been received, workflow started: {trigger}")
+        action_taken = True
 
-
-    elif "email" in trigger:
+    if "email" in trigger:
         send_email(trigger)
+        actions.append("email")
+        action_taken = True
 
-    elif "order" in trigger:
+    if "order" in trigger:
         create_order(trigger)
-    else:
-        print("kindly make another request")
+        actions.append("order")
+        action_taken = True
+    # else:
+    #     print("kindly make another request")
+    if not action_taken:
+        print("kindly input a new request")
 
-
+    print(f"actions detected: {actions}")
